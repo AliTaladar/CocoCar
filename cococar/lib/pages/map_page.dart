ff39ce031cfc8e68f11f75c2a19970985097e509
Ali Taladar <taladarali@gmail.com>
2024-02-04 00:48:49 -0500

    Feat: Track user location

--- cococar/lib/pages/map_page.dart
+++ cococar/lib/pages/map_page.dart
@@ -1,4 +1,9 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'dart:async';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +13,78 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  Location _locationController = new Location();
+
+
+  static const LatLng _sourceLocation = LatLng(45.5019, -73.5674);
+  static const LatLng _destinationLocation = LatLng(45.4914, -73.5873);
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return _currentP == null
+        ? const Center(
+            child: Text("Loading..."),
+          )
+        : Scaffold(
+            body: GoogleMap(
+              initialCameraPosition: const CameraPosition(
+                target: _sourceLocation,
+                zoom: 11,
+              ),
+              markers: {
+                Marker(
+                    markerId: MarkerId("_currentLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _currentP!),
+                const Marker(
+                    markerId: MarkerId("_sourceLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _destinationLocation),
+                const Marker(
+                    markerId: MarkerId("_destinationLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _sourceLocation),
+              },
+            ),
+          );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          print(_currentP);
+        });
+      }
+    });
   }
 }
